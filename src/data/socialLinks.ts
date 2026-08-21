48dcdf58a3f7efb6c704b385601900591c8b864a
edmcclure89 <edmcclure89@gmail.com>
2026-08-21 14:39:21 -0400

    Update socialLinks.ts

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -2,7 +2,7 @@ import { FaInstagram, FaTiktok, FaFacebookF, FaLinkedinIn, FaYoutube } from 'rea
 
 export const socialLinks = [
   { label: 'Instagram', href: 'https://www.instagram.com/swoon_plans/', Icon: FaInstagram },
-  { label: 'TikTok', href: 'https://www.tiktok.com/@4lovecoach_4', Icon: FaTiktok },
+  { label: 'TikTok', href: 'https://www.tiktok.com/@swoon_plans', Icon: FaTiktok },
   { label: 'Facebook', href: 'https://www.facebook.com/profile.php?id=61592905060139', Icon: FaFacebookF },
   { label: 'LinkedIn', href: 'https://www.linkedin.com/company/137294183/', Icon: FaLinkedinIn },
   { label: 'YouTube', href: 'https://www.youtube.com/@SwoonPlans', Icon: FaYoutube },
